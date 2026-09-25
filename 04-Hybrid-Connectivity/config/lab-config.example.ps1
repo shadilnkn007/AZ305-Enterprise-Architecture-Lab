7f790a1dfe81354dfa94838b38c3158f78420e19
shadilnkn007 <54212594+shadilnkn007@users.noreply.github.com>
2026-09-25 21:56:47 +0530

    Create lab-config.example.ps1

---
+++ 04-Hybrid-Connectivity/config/lab-config.example.ps1
@@ -0,0 +1,11 @@
+# ============================================================
+# AZ-305 Enterprise Architecture Lab
+# Module 04 - Hybrid Connectivity
+# ============================================================
+
+# Connectivity subscription contains:
+# - Simulated on-premises VNet
+# - Central India Hub VNet
+# - VPN Gateways
+
+$ConnectivitySubscriptionId = "<YOUR-CONNECTIVITY-SUBSCRIPTION-ID>"
